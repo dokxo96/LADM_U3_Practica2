6e5a9c9643757c50eadc68cf8082b75e150dea51
daniel <dokxo96@gmail.com>
2020-05-06 23:06:35 -0600

    initial commit

---
+++ app/src/main/java/mx/edu/itt/ladm_u3_practica2/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.itt.ladm_u3_practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
